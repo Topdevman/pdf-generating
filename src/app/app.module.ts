184abdf33dbe89295cd1d94f3146d106245079e3
Topdevman <pkpavlo27@yahoo.com>
2017-02-27 04:44:44 -0800

    to add MEAN with angular 2 pdf generator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,36 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { PdfgenComponent } from './pdfgen/pdfgen.component';
+
+import { RouterModule } from '@angular/router';
+import { PdfService } from './pdf.service';
+
+// Define the routers
+const ROUTES = [
+  {
+    path: '',
+    redirectTo: 'createpdf',
+    pathMatch: 'full'
+  },
+  {
+    path: 'createpdf',
+    component: PdfgenComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PdfgenComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(ROUTES) // Add routes to the app
   ],
-  providers: [],
+  providers: [PdfService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
